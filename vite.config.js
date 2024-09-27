7a735e105ad3208603927c7438be48a665d55d74
Eduardo Velez <166083895+Massogon@users.noreply.github.com>
2024-09-27 01:32:50 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
+    host: '0.0.0.0', // Ensure Vite binds to the correct host
+    port: process.env.PORT || 3000, // Use Render's port or default to 3000
+    open: true, // Automatically open the app
   },
-})
+});
